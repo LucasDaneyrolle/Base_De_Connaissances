db0c747fa7f147d70a3d35692e2373cac407fe6e
Dorian Szenberg <72008763+Dorian-Szenberg@users.noreply.github.com>
2022-09-27 11:27:51 +0200

    Création des catégories du formulaire lors de la création d'une fich

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use DateTimeImmutable;
 use App\Entity\Form;
+use App\Entity\Category;
+use App\Entity\FormCategory;
 use App\Form\FicheType;
 use DateTime;
+use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,12 +16,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
 
-
 class FormController extends AbstractController
 {
     #[Route('/form', name: 'app_form')]
 
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, CategoryRepository $categoryRepository): Response
     {
         $objFiche = new Form();
         $objForm = $this->createForm(FicheType::class, $objFiche);
@@ -29,19 +31,28 @@ class FormController extends AbstractController
 
         if ($objForm->isSubmitted() && $objForm->isValid()) {
             $objUser = $this->getUser();
-            $tbaCategories = $objForm->get("categorie")->getData();
             $dthNow = new DateTimeImmutable('now');
+            $tbaCategories = $objForm->get("categorie")->getData();
 
             $objFiche->setState(2);
             $objFiche->setCreatedAt($dthNow);
             $objFiche->setUser($objUser);
 
-            // foreach($tbaCategories as $intClef => $intID) {
-            //     $objFiche->addCategoryForm($intID);
-            // }
-
             $entityManager->persist($objFiche);
             $entityManager->flush();
+
+            foreach($tbaCategories as $intClef => $intID) {
+                $objCategory = new Category();
+                $objFormCategory = new FormCategory();
+
+                $objParam = $objCategory->fetchByID($intID, $categoryRepository);
+
+                $objFormCategory->setCategory($objParam);
+                $objFormCategory->setFiche($objFiche);
+
+                $entityManager->persist($objFormCategory);
+                $entityManager->flush();
+            }
 
             return $this->redirectToRoute('app_form');
         }
