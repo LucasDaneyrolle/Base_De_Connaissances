fff18c4d03ef7b6f25ebc52ef10af51f41069fbe
LoicMo43 <ryyyansaiyajin@gmail.com>
2022-11-09 20:31:46 +0100

    Remove Symfony UX

--- config/bundles.php
+++ config/bundles.php
@@ -15,6 +15,4 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
-    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
